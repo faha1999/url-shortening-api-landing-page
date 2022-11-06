c732ce4bd1d8ea5377ff41f2db3826106fad2299
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-11-06 19:29:49 +0600

    complete Statistic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { ShortenLink } from './components/ShortenLink';
+import { Statistic } from './components/Statistic';
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <Header />
       <Hero />
       <ShortenLink />
+      <Statistic />
     </>
   );
 };
