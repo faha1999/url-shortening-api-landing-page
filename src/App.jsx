32d7cdf2668a074f86bdf72e1434323b9e3bbf3b
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-11-06 19:40:07 +0600

    complete NewsLetter

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { ShortenLink } from './components/ShortenLink';
 import { Statistic } from './components/Statistic';
+import { NewsLetter } from './components/NewsLetter';
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <Hero />
       <ShortenLink />
       <Statistic />
+      <NewsLetter />
     </>
   );
 };
---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NewsLetter = () => {
+  return (
+    <section className="NewsLetter">
+      <div className="container">
+        <h1 className="text-white">Boost your links today</h1>
+        <button className="btn btn-info rounded-pill">Get Started</button>
+      </div>
+    </section>
+  );
+};
